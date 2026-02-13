e190e41efa8b528227710454f8a4e2adf69f8821
LL zhang <zhang31666@outlook.com>
2026-02-13 10:57:34 +0800

    feat: add setting page content

--- JiuMi/ViewModels/SettingPageViewModel.cs
+++ JiuMi/ViewModels/SettingPageViewModel.cs
@@ -1,10 +1,13 @@
-﻿using FluentAvalonia.UI.Controls;
+﻿ using Avalonia.Styling;
+using CommunityToolkit.Mvvm.ComponentModel;
+using FluentAvalonia.UI.Controls;
 using JiuMi.Assets.I18n;
 using JiuMi.Attributes;
+using System.Collections.Generic;
 
 namespace JiuMi.ViewModels;
 
-public class SettingPageViewModel : MainPageViewModelBase
+public partial class SettingPageViewModel : MainPageViewModelBase
 {
     public override string PageKey => "SettingPage";
 
@@ -16,4 +19,18 @@ public class SettingPageViewModel : MainPageViewModelBase
     public override Symbol IconFilledKey => Symbol.SettingsFilled;
 
     public override bool ShowsInFooter => true;
+
+    public List<string> Themes { get; } = [nameof(ThemeVariant.Default), nameof(ThemeVariant.Light), nameof(ThemeVariant.Dark)];
+
+    [ObservableProperty]
+    private string _selectedTheme;
+
+    public Dictionary<string, string> Languages { get; } = new()
+    {
+        { "zh-CN", "简体中文" },
+        { "en-US", "English" }
+    };
+
+    [ObservableProperty]
+    private string _selectedLanguage;
 }
